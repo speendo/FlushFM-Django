9de0d3ef31a9e5715830f05b383d8d6edb5ea6f8
Marcel Jira <marcel@notebook-marcel>
2014-03-09 23:41:22 +0100

    some progress in the create form - still not working perfectly

--- stations/forms.py
+++ stations/forms.py
@@ -1,11 +1,11 @@
 from django.forms import ModelForm
 from django.forms.models import inlineformset_factory
 
-from stations.models import Station, Genre, Address
+from stations.models import Station, StationGenre, Address
 
 class StationForm(ModelForm):
     class Meta:
         model = Station
 
-#GenreFormSet = inlineformset_factory(Station, Genre)
+GenreFormSet = inlineformset_factory(Station, StationGenre)
 AddressFormSet = inlineformset_factory(Station, Address)
--- stations/views.py
+++ stations/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from stations.models import Station
-#from stations.forms import StationForm, GenreFormSet, AddressFormSet
-from stations.forms import StationForm, AddressFormSet
+from stations.forms import StationForm, GenreFormSet, AddressFormSet
 
 # List view
 from django.views.generic import ListView
@@ -24,11 +23,11 @@ def get(self, request, *args, **kwargs):
         self.object = None
         form_class = self.get_form_class()
         form = self.get_form(form_class)
-#        genre_form = GenreFormSet()
+        genre_form = GenreFormSet()
         address_form = AddressFormSet()
         return self.render_to_response(
             self.get_context_data(form=form,
-#                                  genre_form=genre_form,
+                                  genre_form=genre_form,
                                   address_form=address_form))
         
 def post(self, request, *args, **kwargs):
@@ -40,37 +39,34 @@ def post(self, request, *args, **kwargs):
         self.object = None
         form_class = self.get_form_class()
         form = self.get_form(form_class)
-#        genre_form = GenreFormSet(self.request.POST)
+        genre_form = GenreFormSet(self.request.POST)
         address_form = AddressFormSet(self.request.POST)
-#        if (form.is_valid() and genre_form.is_valid() and address_form.is_valid()):
-        if (form.is_valid() and address_form.is_valid()):
-#            return self.form_valid(form, genre_form, address_form)
+        if (form.is_valid() and genre_form.is_valid() and address_form.is_valid()):
+            return self.form_valid(form, genre_form, address_form)
             return self.form_valid(form, address_form)
         else:
-#            return self.form_invalid(form, genre_form, address_form)
+            return self.form_invalid(form, genre_form, address_form)
             return self.form_invalid(form, address_form)
 
-#def form_valid(self, form, genre_form, address_form):
-def form_valid(self, form, address_form):
+def form_valid(self, form, genre_form, address_form):
         """
         Called if all forms are valid. Creates a Recipe instance along with
         associated Ingredients and Instructions and then redirects to a
         success page.
         """
         self.object = form.save()
-#        genre_form.instance = self.object
-#        genre_form.save()
+        genre_form.instance = self.object
+        genre_form.save()
         address_form.instance = self.object
         address_form.save()
         return HttpResponseRedirect(self.get_success_url())
     
-#def form_invalid(self, form, genre_form, address_form):
-def form_invalid(self, form, address_form):
+def form_invalid(self, form, genre_form, address_form):
         """
         Called if a form is invalid. Re-renders the context data with the
         data-filled forms and errors.
         """
         return self.render_to_response(
             self.get_context_data(form=form,
-#                                  genre_form=genre_form,
+                                  genre_form=genre_form,
                                   address_form=address_form))
